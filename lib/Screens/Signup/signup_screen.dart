356ee0e090a1b286563ac6153bb8ac8c3e566d2d
huancnttuet <huancnttuet@gmail.com>
2022-03-04 16:49:07 +0700

    update

--- lib/Screens/Signup/signup_screen.dart
+++ lib/Screens/Signup/signup_screen.dart
@@ -5,6 +5,8 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('Đăng ký'),
+    );
   }
 }
